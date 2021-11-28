090fa7b40ea7248065f4b81fe8ad7afb515a964e
Eric Storm <estormyweather@gmail.com>
2021-11-27 21:00:12 -0500

    Add trie-based decompressor

--- src/decompressor.rs
+++ src/decompressor.rs
@@ -11,10 +11,73 @@ use crate::types::{NumberLike, UnsignedLike};
 use crate::utils;
 
 #[derive(Clone)]
+pub struct PrefixTrie<T> where T: NumberLike {
+  pub child0: Option<Box<PrefixTrie<T>>>,
+  pub child1: Option<Box<PrefixTrie<T>>>,
+  pub value: Option<PrefixDecompressionInfo<T::Unsigned>>,
+}
+
+impl<T> Default for PrefixTrie<T> where T: NumberLike {
+  fn default() -> Self {
+    PrefixTrie::new()
+  }
+}
+
+impl<T> PrefixTrie<T> where T: NumberLike {
+  pub fn new() -> PrefixTrie<T> {
+    PrefixTrie {
+      child0: None,
+      child1: None,
+      value: None,
+    }
+  }
+
+  pub fn insert(&mut self, key: &[bool], value: &Prefix<T>) {
+    if key.is_empty(){
+      self.value = Some(value.into());
+    } else {
+      let (head, tail) = key.split_at(1);
+      if head[0] {
+        if self.child1.is_none() {
+          self.child1 = Some(Box::new(PrefixTrie::new()));
+        }
+        self.child1.as_mut().unwrap().insert(tail, value);
+      } else {
+        if self.child0.is_none() {
+          self.child0 = Some(Box::new(PrefixTrie::new()));
+        }
+        self.child0.as_mut().unwrap().insert(tail, value);
+      }
+    }
+  }
+
+  pub fn contains_key(&self, key: &bool) -> bool {
+    if *key {
+      self.child1.is_some()
+    } else {
+      self.child0.is_some()
+    }
+  }
+
+  pub fn is_leaf(&self) -> bool {
+    self.child0.is_none() && self.child1.is_none()
+  }
+
+  pub fn select_child(&self, key: &bool) -> &PrefixTrie<T> {
+    if *key {
+      self.child1.as_ref().unwrap()
+    } else {
+      self.child0.as_ref().unwrap()
+    }
+  }
+}
+
+#[derive(Clone)]
 pub struct Decompressor<T> where T: NumberLike {
   prefixes: Vec<Prefix<T>>,
   prefix_map: Vec<PrefixDecompressionInfo<T::Unsigned>>,
   prefix_len_map: Vec<u32>,
+  prefix_trie: PrefixTrie<T>,
   max_depth: u32,
   n: usize,
   is_single_prefix: bool,
@@ -39,11 +102,18 @@ impl<T> Decompressor<T> where T: NumberLike {
       prefix_len_map[i] = p.val.len() as u32;
     }
 
+    let mut prefix_trie = PrefixTrie::new();
+    for p in &prefixes {
+      let vals = &p.val;
+      prefix_trie.insert(vals, p);
+    }
+
     let is_single_prefix = prefixes.len() == 1;
     Decompressor {
       prefixes,
       prefix_map,
       prefix_len_map,
+      prefix_trie,
       max_depth,
       n,
       is_single_prefix,
@@ -94,20 +164,12 @@ impl<T> Decompressor<T> where T: NumberLike {
   }
 
   fn next_prefix(&self, reader: &mut BitReader) -> PrefixDecompressionInfo<T::Unsigned> {
-    if self.is_single_prefix {
-      self.prefix_map[0]
-    } else {
-      let mut prefix_idx = 0;
-      for prefix_len in 1..self.max_depth + 1 {
-        if reader.read_one() {
-          prefix_idx |= 1 << (self.max_depth - prefix_len);
-        }
-        if self.prefix_len_map[prefix_idx] == prefix_len {
-          return self.prefix_map[prefix_idx];
-        }
+      let mut prefix_trie = &self.prefix_trie;
+      while !prefix_trie.is_leaf() {
+        let val = reader.read_one();
+        prefix_trie = prefix_trie.select_child(&val);
       }
-      panic!("prefixes are corrupt");
-    }
+      prefix_trie.value.unwrap()
   }
 
   pub fn decompress_n(&self, reader: &mut BitReader, n: usize) -> Vec<T> {
