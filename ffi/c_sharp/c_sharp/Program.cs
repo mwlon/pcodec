67259ecbe82defa227d37455288998965cebe2c5
mwlon <m.w.loncaric@gmail.com>
2022-09-21 20:10:32 -0400

    simplify ffi slightly

--- ffi/c_sharp/c_sharp/Program.cs
+++ ffi/c_sharp/c_sharp/Program.cs
@@ -4,33 +4,28 @@ namespace QCompress {
     class Native
     {
         [DllImport("q_compress_ffi.dylib")]
-        public unsafe static extern CVec auto_compress_i32(
+        public unsafe static extern FfiVec auto_compress_i32(
             int* nums,
             uint len,
             uint level
         );
 
         [DllImport("q_compress_ffi.dylib")]
-        public unsafe static extern void move_compressed_into_buffer(
-            CVec c_vec,
-            byte* buffer
-        );
+        public unsafe static extern void free_compressed(FfiVec ffi_vec);
 
         [DllImport("q_compress_ffi.dylib")]
-        public unsafe static extern CVec auto_decompress_i32(
+        public unsafe static extern FfiVec auto_decompress_i32(
             byte* compressed,
             uint len
         );
 
         [DllImport("q_compress_ffi.dylib")]
-        public unsafe static extern void move_i32_into_buffer(
-            CVec c_vec,
-            int* buffer
-        );
+        public unsafe static extern void free_i32(FfiVec c_vec);
 
-        public struct CVec
+        public struct FfiVec
         {
-            public UIntPtr vec;
+            private UIntPtr raw_box;
+            public UIntPtr ptr;
             public uint len;
         }
     }
@@ -41,32 +36,34 @@ namespace QCompress {
             int[] nums,
             uint level)
         {
-            Native.CVec c_vec;
+            Native.FfiVec ffi_vec;
             fixed (int* nums_ptr = nums)
             {
-                c_vec = Native.auto_compress_i32(nums_ptr, ((uint)nums.Length), level);
+                ffi_vec = Native.auto_compress_i32(nums_ptr, ((uint)nums.Length), level);
             };
-            byte[] buffer = new byte[c_vec.len];
-            fixed (byte* buffer_ptr = buffer)
+            byte[] buffer = new byte[ffi_vec.len];
+            for (int i = 0; i < ffi_vec.len; i++)
             {
-                Native.move_compressed_into_buffer(c_vec, buffer_ptr);
+                buffer[i] = (*(byte*) (ffi_vec.ptr + sizeof(byte) * i));
             }
+            Native.free_compressed(ffi_vec);
             return buffer;
         }
 
         unsafe static int[] auto_decompress_i32(
             byte[] compressed)
         {
-            Native.CVec c_vec;
+            Native.FfiVec ffi_vec;
             fixed (byte* compressed_ptr = compressed)
             {
-                c_vec = Native.auto_decompress_i32(compressed_ptr, ((uint)compressed.Length));
+                ffi_vec = Native.auto_decompress_i32(compressed_ptr, ((uint)compressed.Length));
             };
-            int[] buffer = new int[c_vec.len];
-            fixed (int* buffer_ptr = buffer)
+            int[] buffer = new int[ffi_vec.len];
+            for (int i = 0; i < ffi_vec.len; i++)
             {
-                Native.move_i32_into_buffer(c_vec, buffer_ptr);
+                buffer[i] = (*(int*)(ffi_vec.ptr + sizeof(int) * i));
             }
+            Native.free_i32(ffi_vec);
             return buffer;
         }
 
