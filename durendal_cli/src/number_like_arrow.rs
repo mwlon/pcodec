167b09d5bcabaa1e13a78b65469bbc5170bf0041
mwlon <m.w.loncaric@gmail.com>
2023-07-01 08:47:31 -0400

    simpler

--- durendal_cli/src/number_like_arrow.rs
+++ durendal_cli/src/number_like_arrow.rs
@@ -16,11 +16,11 @@ macro_rules! trivial_arrow {
       type Num = $t;
 
       fn native_to_num(native: Self::Native) -> Self::Num {
-        native
+        native as Self::Num
       }
 
       fn num_to_native(num: Self::Num) -> Self::Native {
-        num
+        num as Self::Native
       }
     }
   };
@@ -28,33 +28,13 @@ macro_rules! trivial_arrow {
 
 trivial_arrow!(f32, arrow_dtypes::Float32Type);
 trivial_arrow!(f64, arrow_dtypes::Float64Type);
+
+trivial_arrow!(i32, arrow_dtypes::Int16Type);
 trivial_arrow!(i32, arrow_dtypes::Int32Type);
 trivial_arrow!(i64, arrow_dtypes::Int64Type);
 trivial_arrow!(i64, arrow_dtypes::TimestampMicrosecondType);
 trivial_arrow!(i64, arrow_dtypes::TimestampNanosecondType);
+
+trivial_arrow!(u32, arrow_dtypes::UInt16Type);
 trivial_arrow!(u32, arrow_dtypes::UInt32Type);
 trivial_arrow!(u64, arrow_dtypes::UInt64Type);
-
-impl NumberLikeArrow for arrow_dtypes::Int16Type {
-  type Num = i32;
-
-  fn native_to_num(native: Self::Native) -> Self::Num {
-    native as i32
-  }
-
-  fn num_to_native(num: Self::Num) -> Self::Native {
-    num as i16
-  }
-}
-
-impl NumberLikeArrow for arrow_dtypes::UInt16Type {
-  type Num = u32;
-
-  fn native_to_num(native: Self::Native) -> Self::Num {
-    native as u32
-  }
-
-  fn num_to_native(num: Self::Num) -> Self::Native {
-    num as u16
-  }
-}
