4bb8fa58314f4e56007ef5b70b73fbc61b89617b
mwlon <m.w.loncaric@gmail.com>
2023-09-03 11:29:35 -0400

    dead stuff

--- pco/src/progress.rs
+++ pco/src/progress.rs
@@ -1,14 +1,5 @@
-use std::ops::AddAssign;
-
 #[derive(Clone, Copy, Debug, Default)]
 pub struct Progress {
   pub n_processed: usize,  // # of numbers decompressed
   pub finished_page: bool, // all numbers have been decompressed
-}
-
-impl AddAssign for Progress {
-  fn add_assign(&mut self, rhs: Self) {
-    self.n_processed += rhs.n_processed;
-    self.finished_page = rhs.finished_page;
-  }
 }
