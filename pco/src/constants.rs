9281e9f01ddd68bbe9aac2dfe42427c327b2e6b0
Ben <4324055+bzinberg@users.noreply.github.com>
2024-05-21 23:55:15 -0400

    Bump `CURRENT_FORMAT_VERSION`
    As requested in review:
    https://github.com/mwlon/pcodec/pull/193#issuecomment-2123789809

--- pco/src/constants.rs
+++ pco/src/constants.rs
@@ -8,7 +8,7 @@ pub(crate) type Bitlen = u32;
 pub(crate) type Weight = u32;
 
 // compatibility
-pub const CURRENT_FORMAT_VERSION: u8 = 1;
+pub const CURRENT_FORMAT_VERSION: u8 = 2;
 
 // bit lengths
 pub const BITS_TO_ENCODE_ANS_SIZE_LOG: Bitlen = 4;
