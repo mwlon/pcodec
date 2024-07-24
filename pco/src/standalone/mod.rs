94d3bc46b4141fc0e758fdadd0cae761759a8bb2
Martin <m.w.loncaric@gmail.com>
2024-07-24 21:42:37 +0200

    simple compress into (#227)

--- pco/src/standalone/mod.rs
+++ pco/src/standalone/mod.rs
@@ -1,7 +1,7 @@
 pub use compressor::{ChunkCompressor, FileCompressor};
 pub use decompressor::{ChunkDecompressor, FileDecompressor, MaybeChunkDecompressor};
 pub use dtype_or_termination::DataTypeOrTermination;
-pub use simple::{simple_compress, simple_decompress, simple_decompress_into, simpler_compress};
+pub use simple::*;
 
 mod compressor;
 mod constants;
