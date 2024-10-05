a0d5c4c6e62940eb215ebfe0ed90a84f3944db54
Niels Jeppesen <niels@jeppesen.dk>
2024-10-05 10:56:27 +0200

    Changed C-binding data length to `size_t`

--- pco_c/include/cpcodec_generated.h
+++ pco_c/include/cpcodec_generated.h
@@ -7,18 +7,18 @@ typedef enum PcoError {
 
 typedef struct PcoFfiVec {
   const void *ptr;
-  unsigned int len;
+  size_t len;
   const void *raw_box;
 } PcoFfiVec;
 
 enum PcoError pco_simpler_compress(const void *nums,
-                                   unsigned int len,
+                                   size_t len,
                                    unsigned char dtype,
                                    unsigned int level,
                                    struct PcoFfiVec *dst);
 
 enum PcoError pco_simple_decompress(const void *compressed,
-                                    unsigned int len,
+                                    size_t len,
                                     unsigned char dtype,
                                     struct PcoFfiVec *dst);
 
