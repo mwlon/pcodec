4752499a5cdd1224d789d46b05668e41a8128de0
mwlon <m.w.loncaric@gmail.com>
2024-10-15 21:17:02 -0400

    undo pco changes

---
+++ dtype_dispatch/README.md
@@ -0,0 +1,158 @@
+`dtype_dispatch` solves the problem of interop between *generic* and
+*dynamically typed (enum)* containers.
+
+This is a common problem in numerical libraries (think numpy, torch, polars):
+you have a variety of data types and data structures to hold them, but every
+function involves matching an enum or converting from a generic to an enum.
+
+Consider this simple API of a hypothetical numerical library supporting the
+`length` and `add` functions on arrays, plus `new` and `downcast`:
+
+```rust
+use std::{any, mem};
+
+pub trait Dtype: 'static {}
+impl Dtype for i32 {}
+impl Dtype for f32 {}
+
+#[derive(Clone, Debug)]
+pub enum DynArray {
+  I32(Vec<i32>),
+  F32(Vec<f32>),
+}
+
+impl DynArray {
+  pub fn length(&self) -> usize {
+    match self {
+      DynArray::I32(inner) => inner.len(),
+      DynArray::F32(inner) => inner.len(),
+    }
+  }
+
+  pub fn add(&self, other: &DynArray) -> DynArray {
+    match (self, other) {
+      (DynArray::I32(inner), DynArray::I32(other_inner)) => {
+        let added = inner.iter().zip(other_inner).map(|(&a, &b)| a + b).collect::<Vec<_>>();
+        DynArray::I32(added)
+      }
+      (DynArray::F32(inner), DynArray::F32(other_inner)) => {
+        let added = inner.iter().zip(other_inner).map(|(&a, &b)| a + b).collect::<Vec<_>>();
+        DynArray::F32(added)
+      }
+      _ => panic!("mismatched dtypes")
+    }
+  }
+
+  pub fn new<T: Dtype>(inner: Vec<T>) -> DynArray {
+    let type_id = any::TypeId::of::<T>();
+    if type_id == any::TypeId::of::<i32>() {
+      DynArray::I32(unsafe { mem::transmute(inner) })
+    } else if type_id == any::TypeId::of::<f32>() {
+      DynArray::F32(unsafe { mem::transmute(inner) })
+    } else {
+      panic!("unknown dtype")
+    }
+  }
+
+  pub fn downcast<T: Dtype>(self) -> Vec<T> {
+    let type_id = any::TypeId::of::<T>();
+    match self {
+      DynArray::I32(inner) => {
+        if type_id == any::TypeId::of::<i32>() {
+          unsafe { mem::transmute(inner) }
+        } else {
+          panic!("incorrect dtype")
+        }
+      }
+      DynArray::F32(inner) => {
+        if type_id == any::TypeId::of::<f32>() {
+          unsafe { mem::transmute(inner) }
+        } else {
+          panic!("incorrect dtype")
+        }
+      }
+    }
+  }
+}
+
+let x_dynamic = DynArray::new(vec![1_i32, 2, 3]);
+let x_doubled_generic = x_dynamic.add( & x_dynamic).downcast::<i32>();
+assert_eq!(x_doubled_generic, vec![2, 4, 6]);
+```
+
+That's a lot of match/if clauses and repeated boilerplate!
+It would become impossible to manage if we had 10 data types and multiple
+containers (e.g. sparse arrays).
+`dtype_dispatch` elegantly solves this with a single macro that generates two
+powerful macros for you to use.
+These building blocks can solve any dynamic<->generic dispatch problem:
+
+```rust
+pub trait Dtype: 'static {}
+impl Dtype for i32 {}
+impl Dtype for f32 {}
+
+// register our two macros, `define_an_enum` and `match_an_enum`, constrained
+// to the `Dtype` trait, with our variant => type mapping:
+dtype_dispatch::build_dtype_macros!(
+  define_an_enum,
+  match_an_enum,
+  Dtype,
+  {
+    I32 => i32,
+    F32 => f32,
+  },
+);
+
+// define any enum for any `Vec` of a data type!
+define_an_enum!(
+  #[derive(Clone, Debug)]
+  DynArray,
+  Vec
+);
+
+impl DynArray {
+  pub fn length(&self) -> usize {
+    match_an_enum!(self, DynArray<T>(inner) => { inner.len() })
+  }
+
+  pub fn add(&self, other: &DynArray) -> DynArray {
+    match_an_enum!(self, DynArray<T>(inner) => {
+      let other_inner = other.downcast_ref::<T>();
+      let added = inner.iter().zip(other_inner).map(|(a, b)| a + b).collect::<Vec<_>>();
+      DynArray::from(added)
+    })
+  }
+}
+
+// we could also use `DynArray::I32()` here, but just to show we can convert generics:
+let x_dynamic = DynArray::from(vec![1_i32, 2, 3]);
+let x_doubled_generic = x_dynamic.add( & x_dynamic).downcast::<i32>();
+assert_eq!(x_doubled_generic, vec![2, 4, 6]);
+```
+
+## Comparisons
+
+|                             | `Box<dyn>` | `enum_dispatch` | `dtype_dispatch`        |
+|-----------------------------|------------|-----------------|-------------------------|
+| convert generic -> dynamic  | ✅          | ❌*              | ✅                       |
+| convert dynamic -> generic  | ❌          | ❌*              | ✅                       |
+| call trait fns directly     | ⚠️**       | ✅               | ❌                       |
+| match with type information | ❌️         | ❌               | ✅                       |
+| stack allocated             | ❌️         | ✅               | ✅                       |
+| variant type requirements   | trait impl | trait impl      | container\<trait impl\> |
+
+*Although `enum_dispatch` supports `From` and `TryInto`, it only works for
+concrete types (not in generic contexts).
+
+**Trait objects can only dispatch to functions that can be put in a vtable,
+which is annoyingly restrictive.
+For instance, traits with generic associated functions can't be put in a
+`Box<dyn>`.
+
+## Limitations
+
+At present, enum and container type names must always be a single identifier.
+For instance, `Vec` will work, but `std::vec::Vec` will not.
+You can satisfy this by `use`ing your type or making a type alias of it,
+e.g. `type MyContainer<T: MyConstraint> = Vec<Foo<T>>`.
---
+++ dtype_dispatch/src/lib.rs
@@ -0,0 +1,164 @@
+#![doc = include_str!("../README.md")]
+
+/// Produces two macros: an enum definer and an enum matcher.
+///
+/// See the crate-level documentation for more info.
+#[macro_export]
+macro_rules! build_dtype_macros {
+    (
+        $definer: ident,
+        $matcher: ident,
+        $constraint: path,
+        {$($variant: ident => $t: ty,)+}$(,)?
+    ) => {
+        macro_rules! $definer {
+            (#[$attrs: meta] $vis: vis $name: ident, $container: ident) => {
+                $vis trait Downcast {
+                    fn downcast<S: $constraint>(self) -> $container<S>;
+                    fn downcast_ref<S: $constraint>(&self) -> &$container<S>;
+                    fn downcast_mut<S: $constraint>(&mut self) -> &mut $container<S>;
+                }
+
+                #[$attrs]
+                $vis enum $name {
+                    $($variant($container<$t>),)+
+                }
+
+                impl<T: $constraint> Downcast for $container<T> {
+                    fn downcast<S: $constraint>(self) -> $container<S> {
+                        if std::any::TypeId::of::<S>() == std::any::TypeId::of::<T>() {
+                            unsafe {
+                                std::mem::transmute::<_, $container<S>>(self)
+                            }
+                        } else {
+                            panic!(
+                                "unsafe downcast conversion from {} to {}",
+                                std::any::type_name::<T>(),
+                                std::any::type_name::<S>(),
+                            )
+                        }
+                    }
+
+                    fn downcast_ref<S: $constraint>(&self) -> &$container<S> {
+                        if std::any::TypeId::of::<S>() == std::any::TypeId::of::<T>() {
+                            unsafe {
+                                std::mem::transmute::<_, &$container<S>>(self)
+                            }
+                        } else {
+                            panic!(
+                                "unsafe downcast conversion from {} to {}",
+                                std::any::type_name::<T>(),
+                                std::any::type_name::<S>(),
+                            )
+                        }
+                    }
+
+                    fn downcast_mut<S: $constraint>(&mut self) -> &mut $container<S> {
+                        if std::any::TypeId::of::<S>() == std::any::TypeId::of::<T>() {
+                            unsafe {
+                                std::mem::transmute::<_, &mut $container<S>>(self)
+                            }
+                        } else {
+                            panic!(
+                                "unsafe downcast conversion from {} to {}",
+                                std::any::type_name::<T>(),
+                                std::any::type_name::<S>(),
+                            )
+                        }
+                    }
+                }
+
+                impl $name {
+                    pub fn downcast<S: $constraint>(self) -> $container<S> {
+                        match self {
+                            $(
+                                Self::$variant(inner) => inner.downcast::<S>(),
+                            )+
+                        }
+                    }
+
+                    pub fn downcast_ref<S: $constraint>(&self) -> &$container<S> {
+                        match self {
+                            $(
+                                Self::$variant(inner) => inner.downcast_ref::<S>(),
+                            )+
+                        }
+                    }
+
+                    pub fn downcast_mut<S: $constraint>(&mut self) -> &mut $container<S> {
+                        match self {
+                            $(
+                                Self::$variant(inner) => inner.downcast_mut::<S>(),
+                            )+
+                        }
+                    }
+                }
+
+                impl<S: $constraint> From<$container<S>> for $name {
+                    fn from(value: $container<S>) -> Self {
+                        let type_id = std::any::TypeId::of::<S>();
+                        $(
+                            if type_id == std::any::TypeId::of::<$t>() {
+                                return $name::$variant(value.downcast());
+                            }
+                        )+
+                        panic!("unsafe conversion from {}", std::any::type_name::<S>());
+                    }
+                }
+            };
+        }
+
+        macro_rules! $matcher {
+            ($value: expr, $enum_: ident<$generic: ident>($inner: ident) => $block: block) => {
+                match $value {
+                    $($enum_::$variant($inner) => {
+                        type $generic = $t;
+                        $block
+                    })+
+                }
+            };
+        }
+    };
+}
+
+#[allow(dead_code)]
+#[cfg(test)]
+mod tests {
+  trait Constraint: 'static {}
+
+  impl Constraint for u16 {}
+  impl Constraint for u32 {}
+  impl Constraint for u64 {}
+
+  build_dtype_macros!(
+      define_enum,
+      match_enum,
+      crate::tests::Constraint,
+      {
+          U16 => u16,
+          U32 => u32,
+          U64 => u64,
+      }
+  );
+
+  define_enum!(
+    #[derive(Clone, Debug)]
+    MyEnum,
+    Vec
+  );
+
+  // we use this helper just to prove that we can handle generic types, not
+  // just concrete types
+  fn generic_new<T: Constraint>(inner: Vec<T>) -> MyEnum {
+    MyEnum::from(inner)
+  }
+
+  #[test]
+  fn test_end_to_end() {
+    let x = generic_new(vec![1_u16, 1, 2, 3, 5]);
+    let bit_size = match_enum!(&x, MyEnum<L>(inner) => { inner.len() * L::BITS as usize });
+    assert_eq!(bit_size, 80);
+    let x = x.downcast::<u16>();
+    assert_eq!(x[0], 1);
+  }
+}
