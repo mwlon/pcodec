e4c46c55ae08cb35377924a204440e9de9d28a97
mwlon <m.w.loncaric@gmail.com>
2024-10-20 15:58:42 -0400

    maybe update c api

--- pco_c/src/lib.rs
+++ pco_c/src/lib.rs
@@ -4,9 +4,9 @@ use std::ptr;
 
 use libc::{c_uchar, c_uint, c_void, size_t};
 
-use pco::data_types::{CoreDataType, NumberLike};
-
 use crate::PcoError::PcoInvalidType;
+use pco::data_types::{CoreDataType, NumberLike};
+use pco::match_number_like_enum;
 
 #[repr(C)]
 pub enum PcoError {
@@ -17,36 +17,10 @@ pub enum PcoError {
   PcoDecompressionError,
 }
 
-macro_rules! impl_dtypes {
-  {$($names:ident($lname:ident) => $types:ty,)+} => {
-    // tell rust to ignore these warnings because we rely on the memory layout in C
-    #[allow(dead_code)]
-    enum DynTypedVec {
-      U8(Vec<u8>),
-      $($names(Vec<$types>),)+
-    }
-
-    impl From<Vec<u8>> for DynTypedVec {
-      fn from(vec: Vec<u8>) -> DynTypedVec { DynTypedVec::U8(vec) }
-    }
-
-    $(
-      impl From<Vec<$types>> for DynTypedVec {
-        fn from(vec: Vec<$types>) -> DynTypedVec { DynTypedVec::$names(vec) }
-      }
-    )+
-
-    macro_rules! match_dtype {
-      ($matcher:expr, $fn:ident, $params:tt) => {
-        match $matcher {
-          $(CoreDataType::$names => $fn::<$types>$params,)+
-        }
-      }
-    }
-  }
-}
-
-pco::with_core_dtypes!(impl_dtypes);
+pco::define_number_like_enum!(
+  #[derive()]
+  NumVec(Vec)
+);
 
 #[repr(C)]
 pub struct PcoFfiVec {
@@ -56,20 +30,21 @@ pub struct PcoFfiVec {
 }
 
 impl PcoFfiVec {
-  fn init_from_vec<T>(&mut self, v: Vec<T>)
-  where
-    Vec<T>: Into<DynTypedVec>,
-  {
+  fn init_from_bytes(&mut self, v: Vec<u8>) {
     self.ptr = v.as_ptr() as *const c_void;
     self.len = v.len();
-    self.raw_box = Box::into_raw(Box::new(v.into())) as *const c_void;
+    self.raw_box = Box::into_raw(v.into_boxed_slice()) as *const c_void;
+  }
+
+  fn init_from_nums<T: NumberLike>(&mut self, v: Vec<T>) {
+    self.ptr = v.as_ptr() as *const c_void;
+    self.len = v.len();
+    self.raw_box = Box::into_raw(v.into_boxed_slice()) as *const c_void;
   }
 
   fn free(&mut self) {
     unsafe {
-      drop(Box::from_raw(
-        self.raw_box as *mut DynTypedVec,
-      ));
+      drop(Box::from_raw(self.raw_box as *mut NumVec));
     }
     self.ptr = ptr::null();
     self.len = 0;
@@ -87,7 +62,7 @@ fn _simpler_compress<T: NumberLike>(
   match pco::standalone::simpler_compress(slice, level as usize) {
     Err(_) => PcoError::PcoCompressionError,
     Ok(v) => {
-      unsafe { (*ffi_vec_ptr).init_from_vec(v) };
+      unsafe { (*ffi_vec_ptr).init_from_bytes(v) };
       PcoError::PcoSuccess
     }
   }
@@ -97,15 +72,12 @@ fn _simple_decompress<T: NumberLike>(
   compressed: *const c_void,
   len: size_t,
   ffi_vec_ptr: *mut PcoFfiVec,
-) -> PcoError
-where
-  Vec<T>: Into<DynTypedVec>,
-{
+) -> PcoError {
   let slice = unsafe { std::slice::from_raw_parts(compressed as *const u8, len) };
   match pco::standalone::simple_decompress::<T>(slice) {
     Err(_) => PcoError::PcoDecompressionError,
     Ok(v) => {
-      unsafe { (*ffi_vec_ptr).init_from_vec(v) };
+      unsafe { (*ffi_vec_ptr).init_from_nums(v) };
       PcoError::PcoSuccess
     }
   }
@@ -123,10 +95,11 @@ pub extern "C" fn pco_simpler_compress(
     return PcoInvalidType;
   };
 
-  match_dtype!(
+  match_number_like_enum!(
     dtype,
-    _simpler_compress,
-    (nums, len, level, dst)
+    CoreDataType<T> => {
+      _simpler_compress::<T>(nums, len, level, dst)
+    }
   )
 }
 
@@ -141,10 +114,11 @@ pub extern "C" fn pco_simple_decompress(
     return PcoInvalidType;
   };
 
-  match_dtype!(
+  match_number_like_enum!(
     dtype,
-    _simple_decompress,
-    (compressed, len, dst)
+    CoreDataType<T> => {
+      _simple_decompress::<T>(compressed, len, dst)
+    }
   )
 }
 
