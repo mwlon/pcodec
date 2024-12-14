c6286616d8f560ba9392c30b9b7d760229679e13
mwlon <m.w.loncaric@gmail.com>
2024-12-14 12:31:21 -0500

    turbo pfor + zstd

--- pco_cli/src/bench/codecs/turbo_pfor.rs
+++ pco_cli/src/bench/codecs/turbo_pfor.rs
@@ -1,6 +1,7 @@
 use std::convert::TryInto;
 use std::mem;
 
+use anyhow::Result;
 use clap::Parser;
 use half::f16;
 
@@ -8,7 +9,17 @@ use crate::bench::codecs::CodecInternal;
 use crate::dtypes::{PcoNumber, TurboPforable};
 
 #[derive(Clone, Debug, Parser)]
-pub struct TurboPforConfig {}
+pub struct TurboPforConfig {
+  #[arg(long)]
+  zstd_level: Option<i32>,
+}
+
+fn optional_int_to_string(x: Option<i32>) -> String {
+  match x {
+    Some(x) => x.to_string(),
+    None => "none".to_string(),
+  }
+}
 
 impl CodecInternal for TurboPforConfig {
   fn name(&self) -> &'static str {
@@ -16,7 +27,10 @@ impl CodecInternal for TurboPforConfig {
   }
 
   fn get_confs(&self) -> Vec<(&'static str, String)> {
-    vec![]
+    vec![(
+      "zstd-level",
+      optional_int_to_string(self.zstd_level),
+    )]
   }
 
   fn compress<T: PcoNumber>(&self, nums: &[T]) -> Vec<u8> {
@@ -27,10 +41,23 @@ impl CodecInternal for TurboPforConfig {
     dst[..8].copy_from_slice(&(nums.len() as u64).to_le_bytes());
     let byte_len = unsafe { <T as TurboPforable>::encode(&mut nums, &mut dst[8..]) };
     dst.truncate(byte_len + 8);
-    dst
+
+    if let Some(zstd_level) = self.zstd_level {
+      zstd::encode_all(dst.as_slice(), zstd_level).unwrap()
+    } else {
+      dst
+    }
   }
 
   fn decompress<T: PcoNumber>(&self, src: &[u8]) -> Vec<T> {
+    let decompressed_src: Vec<u8>;
+    let src = if self.zstd_level.is_some() {
+      decompressed_src = zstd::decode_all(src).unwrap();
+      &decompressed_src
+    } else {
+      src
+    };
+
     let n = u64::from_le_bytes(src[..8].try_into().unwrap()) as usize;
     let mut src = src[8..].to_vec();
     let mut dst = Vec::with_capacity(n);
