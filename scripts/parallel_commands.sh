d9aa0cc29f3bc9e1a60157b6df9016ed0e810051
mwlon <m.w.loncaric@gmail.com>
2025-02-09 09:44:51 -0500

    don't commit scripts

--- scripts/parallel_commands.sh
+++
@@ -1,3 +0,0 @@
-#shuf commands.txt | parallel --jobs 1 "taskset -c 48 sh -c {}"
-shuf commands.txt | parallel --jobs 24 "taskset -c \$(( {%} + 23 )) sh -c {}"
-#shuf commands.txt | parallel --jobs 48 "taskset -c {%} sh -c {}"
